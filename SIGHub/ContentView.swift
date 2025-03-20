f4d308e4f33ea466cd83876858389ee0c8a2fb2b
Ilham <ham>
2025-03-20 11:49:17 +0700

    Initial Commit

---
+++ SIGHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SIGHub
+//
+//  Created by Ilham Shahputra on 20/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SIGHub/SIGHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIGHubApp.swift
+//  SIGHub
+//
+//  Created by Ilham Shahputra on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SIGHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
