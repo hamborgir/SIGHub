aea398eba8a393f1a1c72e4bbd7cee7c0bc7b749
Ethelind Septiani Metta <ethelindseptianimetta@10-60-60-187.0200.bsd.id.iosda.org>
2025-04-09 08:51:45 +0700

    Ubah warna chevron, ubah warna session home page,

--- SIGHub/HomeView.swift
+++ SIGHub/HomeView.swift
@@ -186,6 +186,7 @@ struct SpotlightView: View {
     private var eventList: [EventModel] = EventModel.eventList
     private var isNearestEventAvailable: Bool { !nearestEventList.isEmpty }
     
+    
     private var nearestEventList: [EventModel] {
         let N : Int = 7 // days to consider
         
@@ -299,7 +300,8 @@ struct SIGCategorizedView: View {
                                 .foregroundColor(.black)
                             
                             Image(systemName: "chevron.right")
-                                .foregroundColor(.gray)
+                                .foregroundColor(.accentColor)
+                                .fontWeight(.bold)
                                 .padding(.leading, 0)
                         }
                     }
